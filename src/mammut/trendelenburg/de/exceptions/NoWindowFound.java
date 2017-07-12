c8002e481a7277da319443c25f929bca61da35aa
Sünktaktik-Schuggah <trendelenburger19.04@googlemail.com>
2017-07-12 21:09:41 +0200

    jo

--- src/mammut/trendelenburg/de/exceptions/NoWindowFound.java
+++ src/mammut/trendelenburg/de/exceptions/NoWindowFound.java
@@ -2,4 +2,9 @@ package mammut.trendelenburg.de.exceptions;
 
 public class NoWindowFound extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3984157269009942857L;
+
 }
